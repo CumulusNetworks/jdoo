CumulusNetworks/jdoo

commit 12e10224738fcf43919982ee3207d65ed18e2667

resource: add a new resource monitor called sysloadavg
This patch adds a new resource monitor called sysloadavg which
provides a load average number normalized over the number of actual
processor cores the system has. This provides a bit more predictable
way to determine the actual overall system load as this configuration
doesn't have to change based on what type of system monit is being
run on. The resource supports 15, 5, and 1 minute averages just like
the loadavg resource provides. To use one just needs to add the
following under a "check system" stanza.

  check system localhost
    if sysloadavg (15min) > 110% then <action>

Notice the load average is tested against a percentage rather than
an absolute number. This helps in normalizing the configuration
across varying system configurations.

http/cervlet.c
@@ -1771,6 +1771,18 @@ static void print_service_rules_resource(HttpResponse res, Service_T s) {
         case RESOURCE_ID_LOAD15: 
           out_print(res, "Load average (15min)");
           break;
+
+        case RESOURCE_ID_SYSLOAD1:
+          out_print(res, "System Load average (1min)");
+          break;
+
+        case RESOURCE_ID_SYSLOAD5:
+          out_print(res, "System Load average (5min)");
+          break;
+
+        case RESOURCE_ID_SYSLOAD15:
+          out_print(res, "System Load average (15min)");
+          break;
           
         case RESOURCE_ID_CHILDREN:
           out_print(res, "Children");
@@ -1812,6 +1824,15 @@ static void print_service_rules_resource(HttpResponse res, Service_T s) {
         case RESOURCE_ID_LOAD5:
         case RESOURCE_ID_LOAD15:
           out_print(res, "If %s %.1f %s ", operatornames[q->operator], q->limit / 10.0, Util_getEventratio(a->failed, buf, sizeof(buf)));
+          out_print(res, "then %s ", Util_describeAction(a->failed, buf, sizeof(buf)));
+          out_print(res, "else if succeeded %s ", Util_getEventratio(a->succeeded, buf, sizeof(buf)));
+          out_print(res, "then %s", Util_describeAction(a->succeeded, buf, sizeof(buf)));
+          break;
+
+        case RESOURCE_ID_SYSLOAD1:
+        case RESOURCE_ID_SYSLOAD5:
+        case RESOURCE_ID_SYSLOAD15:
+          out_print(res, "If %s %.1f %s ", operatornames[q->operator], q->limit, Util_getEventratio(a->failed, buf, sizeof(buf)));
           out_print(res, "then %s ", Util_describeAction(a->failed, buf, sizeof(buf)));
           out_print(res, "else if succeeded %s ", Util_getEventratio(a->succeeded, buf, sizeof(buf)));
           out_print(res, "then %s", Util_describeAction(a->succeeded, buf, sizeof(buf)));

l.l
@@ -106,6 +106,9 @@ notequal    ("notequal"|"ne"|"!=")
 loadavg1    load(avg)[ ]*(\([ ]*1[ ]*(m|min)?[ ]*\))?
 loadavg5    load(avg)[ ]*\([ ]*5[ ]*(m|min)?[ ]*\)
 loadavg15   load(avg)[ ]*\([ ]*15[ ]*(m|min)?[ ]*\)
+sysloadavg1    sysload(avg)[ ]*(\([ ]*1[ ]*(m|min)?[ ]*\))?
+sysloadavg5    sysload(avg)[ ]*\([ ]*5[ ]*(m|min)?[ ]*\)
+sysloadavg15   sysload(avg)[ ]*\([ ]*15[ ]*(m|min)?[ ]*\)
 cpuuser     cpu[ ]*(usage)*[ ]*\([ ]*(us|usr|user)?[ ]*\)
 cpusyst     cpu[ ]*(usage)*[ ]*\([ ]*(sy|sys|system)?[ ]*\)
 cpuwait     cpu[ ]*(usage)*[ ]*\([ ]*(wa|wait)?[ ]*\)
@@ -314,6 +317,9 @@ fips              { return FIPS; }
 {loadavg1}        { return LOADAVG1; }
 {loadavg5}        { return LOADAVG5; }
 {loadavg15}       { return LOADAVG15; }
+{sysloadavg1}     { return SYSLOADAVG1; }
+{sysloadavg5}     { return SYSLOADAVG5; }
+{sysloadavg15}    { return SYSLOADAVG15; }
 {cpuuser}         { return CPUUSER; }
 {cpusyst}         { return CPUSYSTEM; }
 {cpuwait}         { return CPUWAIT; }

monitor.h
@@ -184,6 +184,9 @@
 #define RESOURCE_ID_TOTAL_CPU_PERCENT 15
 #define RESOURCE_ID_SWAP_PERCENT      16
 #define RESOURCE_ID_SWAP_KBYTE        17
+#define RESOURCE_ID_SYSLOAD1          18
+#define RESOURCE_ID_SYSLOAD5          19
+#define RESOURCE_ID_SYSLOAD15         20
 
 #define DIGEST_CLEARTEXT   1
 #define DIGEST_CRYPT       2

p.y
@@ -289,6 +289,7 @@
 %token CHECKPROC CHECKFILESYS CHECKFILE CHECKDIR CHECKHOST CHECKSYSTEM CHECKFIFO CHECKSTATUS
 %token CHILDREN SYSTEM
 %token RESOURCE MEMORY TOTALMEMORY LOADAVG1 LOADAVG5 LOADAVG15 SWAP
+%token SYSLOADAVG1 SYSLOADAVG5 SYSLOADAVG15
 %token MODE ACTIVE PASSIVE MANUAL CPU TOTALCPU CPUUSER CPUSYSTEM CPUWAIT
 %token GROUP REQUEST DEPENDS BASEDIR SLOT EVENTQUEUE SECRET HOSTHEADER
 %token UID GID MMONIT INSTANCE USERNAME PASSWORD
@@ -1351,6 +1352,7 @@ resourcesystemlist : resourcesystemopt
                    ;
 
 resourcesystemopt  : resourceload
+                   | resourcesysload
                    | resourcemem
                    | resourceswap
                    | resourcecpu
@@ -1432,6 +1434,18 @@ resourceloadavg : LOADAVG1  { $<number>$ = RESOURCE_ID_LOAD1; }
                 | LOADAVG5  { $<number>$ = RESOURCE_ID_LOAD5; }
                 | LOADAVG15 { $<number>$ = RESOURCE_ID_LOAD15; }
                 ;
+
+resourcesysload : resourcesysloadavg operator NUMBER PERCENT {
+                    resourceset.resource_id = $<number>1;
+                    resourceset.operator = $<number>2;
+                    resourceset.limit = $3;
+                  }
+                ;
+
+resourcesysloadavg : SYSLOADAVG1  { $<number>$ = RESOURCE_ID_SYSLOAD1; }
+                   | SYSLOADAVG5  { $<number>$ = RESOURCE_ID_SYSLOAD5; }
+                   | SYSLOADAVG15 { $<number>$ = RESOURCE_ID_SYSLOAD15; }
+                   ;
 
 value           : REAL { $<real>$ = $1; }
                 | NUMBER { $<real>$ = (float) $1; }

util.c
@@ -959,6 +959,18 @@ void Util_printService(Service_T s) {
         printf(" %-20s = ", "Load avg. (15min)");
         break;
 
+      case RESOURCE_ID_SYSLOAD1:
+        printf(" %-20s = ", "SysLoad avg. (1min)");
+        break;
+
+      case RESOURCE_ID_SYSLOAD5:
+        printf(" %-20s = ", "SysLoad avg. (5min)");
+        break;
+
+      case RESOURCE_ID_SYSLOAD15:
+        printf(" %-20s = ", "SysLoad avg. (15min)");
+        break;
+
       case RESOURCE_ID_CHILDREN:
         printf(" %-20s = ", "Children");
         break;
@@ -1000,6 +1012,19 @@ void Util_printService(Service_T s) {
         printf("if %s %.1f %s ", operatornames[q->operator], q->limit / 10.0, Util_getEventratio(a->failed, buf, sizeof(buf)));
         printf("then %s ", Util_describeAction(a->failed, buf, sizeof(buf)));
         printf("else if succeeded %s ", Util_getEventratio(a->succeeded, buf, sizeof(buf)));
+        printf("then %s", Util_describeAction(a->succeeded, buf, sizeof(buf)));
+        break;
+
+      case RESOURCE_ID_SYSLOAD1:
+      case RESOURCE_ID_SYSLOAD5:
+      case RESOURCE_ID_SYSLOAD15:
+        printf("if %s %.1f %s ",
+            operatornames[q->operator],
+            (double) q->limit,
+            Util_getEventratio(a->failed, buf, sizeof(buf)));
+        printf("then %s ", Util_describeAction(a->failed, buf, sizeof(buf)));
+        printf("else if succeeded %s ",
+            Util_getEventratio(a->succeeded, buf, sizeof(buf)));
         printf("then %s", Util_describeAction(a->succeeded, buf, sizeof(buf)));
         break;
 

validate.c
@@ -661,6 +661,7 @@ static void check_process_resources(Service_T s, Resource_T r) {
 
   int okay = TRUE;
   char report[STRLEN]={0};
+  double sysloadavg = 0.0;
 
   ASSERT(s && r);
 
@@ -790,6 +791,48 @@ static void check_process_resources(Service_T s, Resource_T r) {
       okay = FALSE;
     } else
       snprintf(report, STRLEN, "'%s' loadavg(15min) check succeeded [current loadavg(15min)=%.1f]", s->name, systeminfo.loadavg[2]);
+    break;
+
+  case RESOURCE_ID_SYSLOAD1:
+    sysloadavg = ((systeminfo.loadavg[0] / systeminfo.cpus) * 100.0);
+    if (Util_evalQExpression(r->operator, (int) sysloadavg, r->limit)) {
+      snprintf(report, STRLEN, "sysloadavg(1min) of %.1f matches resource "
+               "limit [sysloadavg(1min)%s%.1f]", sysloadavg,
+               operatorshortnames[r->operator], (double) r->limit);
+      okay = FALSE;
+    } else {
+      snprintf(report, STRLEN, "'%s' sysloadavg(1min) check succeeded "
+               "[current sysloadavg(1min)=%.1f]", s->name,
+               sysloadavg);
+    }
+    break;
+
+  case RESOURCE_ID_SYSLOAD5:
+    sysloadavg = ((systeminfo.loadavg[1] / systeminfo.cpus) * 100.0);
+    if (Util_evalQExpression(r->operator, (int) sysloadavg, r->limit)) {
+      snprintf(report, STRLEN, "sysloadavg(5min) of %.1f matches resource "
+               "limit [sysloadavg(5min)%s%.1f]", sysloadavg,
+               operatorshortnames[r->operator], (double) r->limit);
+      okay = FALSE;
+    } else {
+      snprintf(report, STRLEN, "'%s' sysloadavg(5min) check succeeded "
+               "[current sysloadavg(5min)=%.1f]", s->name,
+               sysloadavg);
+    }
+    break;
+
+  case RESOURCE_ID_SYSLOAD15:
+    sysloadavg = ((systeminfo.loadavg[2] / systeminfo.cpus) * 100.0);
+    if (Util_evalQExpression(r->operator, (int) sysloadavg, r->limit)) {
+      snprintf(report, STRLEN, "sysloadavg(15min) of %.1f matches resource "
+               "limit [sysloadavg(15min)%s%.1f]", sysloadavg,
+               operatorshortnames[r->operator], (double) r->limit);
+      okay = FALSE;
+    } else {
+      snprintf(report, STRLEN, "'%s' sysloadavg(15min) check succeeded "
+               "[current sysloadavg(15min)=%.1f]", s->name,
+               sysloadavg);
+    }
     break;
 
   case RESOURCE_ID_CHILDREN:

commit 9b7c0e3d3543fff15aca289f87a3167970a37b41

sysloadavg: fix SEGFAULT caused by incorrect printf type
Commit 12e10224738fcf43919982ee3207d65ed18e2667 added support for
a new resource "sysloadavg". In http/cervlet.c line 1843 the call
to out_print in the format specifier defined a float or double ("%.1f")
be provided in the 4th argument. The original value provided was of type
int.

The fix is to explicitly typecast the 4th argument to type double.

Closes: https://tickets.cumulusnetworks.com/browse/CM-5345
Fixes: 12e10224738fcf43919982ee3207d65ed18e2667
Signed-off-by: Jonathan Toppins <jtoppins@cumulusnetworks.com>

http/cervlet.c
@@ -1832,7 +1832,7 @@ static void print_service_rules_resource(HttpResponse res, Service_T s) {
         case RESOURCE_ID_SYSLOAD1:
         case RESOURCE_ID_SYSLOAD5:
         case RESOURCE_ID_SYSLOAD15:
-          out_print(res, "If %s %.1f %s ", operatornames[q->operator], q->limit, Util_getEventratio(a->failed, buf, sizeof(buf)));
+          out_print(res, "If %s %.1f %s ", operatornames[q->operator], (double) q->limit, Util_getEventratio(a->failed, buf, sizeof(buf)));
           out_print(res, "then %s ", Util_describeAction(a->failed, buf, sizeof(buf)));
           out_print(res, "else if succeeded %s ", Util_getEventratio(a->succeeded, buf, sizeof(buf)));
           out_print(res, "then %s", Util_describeAction(a->succeeded, buf, sizeof(buf)));